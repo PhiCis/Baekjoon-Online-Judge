7ff885c84d43fb3a1324b2cbb327589f2d5bb9d1
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2021-01-30 00:40:28 +0900

    Add files via upload

---
+++ BOJ 1100 하얀 칸.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int ans, i, j;
+char a;
+int main()
+{
+	for(i=0;i<8;i++){
+		for(j=0;j<8;j++){
+			scanf(" %c", &a);
+			if(a=='F'&&(i+j)%2==0) ans++;
+		}
+	}
+	printf("%d", ans);
+}
