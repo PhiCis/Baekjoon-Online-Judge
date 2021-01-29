01df7d053f9db51c8bde67ab60ffd738452b94ac
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2021-01-29 20:01:53 +0900

    Lazy Spelling Bee (Small)

---
+++ BOJ 14300-14399/BOJ 14330.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	for(int i=1;i<=T;i++)
+	{
+		char a[10]={0};
+		scanf("%s", a);
+		int len=strlen(a);
+		long long ans=1;
+		if(len==1)
+		{
+			printf("Case #%d: %lld\n", i, ans);
+			continue;
+		}
+		for(int j=0;j<len;j++)
+		{
+			if(j==0)
+			{
+				if(a[j]!=a[j+1]) ans=ans*2%MOD;
+			}
+			else if(j==len-1)
+			{
+				if(a[j]!=a[j-1]) ans=ans*2%MOD;
+			}
+			else
+			{
+				if(a[j-1]!=a[j]&&a[j]!=a[j+1]&&a[j+1]!=a[j-1]) ans=ans*3%MOD;
+				else if(a[j-1]!=a[j]||a[j]!=a[j+1]||a[j+1]!=a[j-1]) ans=ans*2%MOD;
+			}
+		}
+		printf("Case #%d: %lld\n", i, ans);
+	}
+}
