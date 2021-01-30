7172121b19f7a526b022aa31bf1b4f9df22c9cc1
Jubin Kim <63911295+PhiCis@users.noreply.github.com>
2021-01-30 12:01:33 +0900

    가장 긴 감소하는 부분 수열

---
+++ BOJ 11700-11799/BOJ 11722.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int N, A[1010], DP[1010], ans;
+int main()
+{
+	scanf("%d", &N);
+	for(int i=1;i<=N;i++)
+	{
+		scanf("%d", &A[i]);
+	}
+	for(int i=1;i<=N;i++)
+	{
+		DP[i]=1;
+		for(int j=1;j<i;j++)
+		{
+			if(A[j]>A[i])
+			{
+				DP[i]=max(DP[i], DP[j]+1);
+			}
+		}
+		if(ans<DP[i]) ans=DP[i];
+	}
+	printf("%d", ans);
+}
